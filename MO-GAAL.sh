95273780741d8d2575c85609631f15c2a087f194
leibinghe <43264058+leibinghe@users.noreply.github.com>
2018-12-05 14:54:19 +0800

    Update MO-GAAL.sh

--- MO-GAAL.sh
+++ MO-GAAL.sh
@@ -1,3 +1,5 @@
+python MO-GAAL.py --path Data/onecluster --k 10 --stop_epochs 1500 --lr_d 0.01 --lr_g 0.0001 --decay 1e-6 --momentum 0.9
+
 python MO-GAAL.py --path Data/Annthyroid --k 10 --stop_epochs 25 --lr_d 0.01 --lr_g 0.0001 --decay 1e-6 --momentum 0.9
 
 python MO-GAAL.py --path Data/WDBC --k 10 --stop_epochs 200 --lr_d 0.01 --lr_g 0.0001 --decay 1e-6 --momentum 0.9
