36cc9d59008aa25678617dd16615a8e859642a07
leibinghe <43264058+leibinghe@users.noreply.github.com>
2018-12-05 14:53:41 +0800

    Update SO-GAAL.sh

--- SO-GAAL.sh
+++ SO-GAAL.sh
@@ -1,3 +1,5 @@
+python SO-GAAL.py --path Data/onecluster --stop_epochs 1000 --lr_d 0.01 --lr_g 0.0001 --decay 1e-6 --momentum 0.9
+
 python SO-GAAL.py --path Data/Annthyroid --stop_epochs 20 --lr_d 0.01 --lr_g 0.0001 --decay 1e-6 --momentum 0.9
 
 python SO-GAAL.py --path Data/WDBC --stop_epochs 240 --lr_d 0.01 --lr_g 0.0001 --decay 1e-6 --momentum 0.9
